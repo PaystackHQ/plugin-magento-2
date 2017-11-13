27b3027b39068724913ad3ecf1b80f02b2d415a0
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2017-11-13 15:26:04 +0100

    Added the Observer class to handle the 'payment_verify_after' event

---
+++ Observer/AfterPaymentVerifyObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Profibro\Paystack\Observer;
+
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Model\Order;
+
+class AfterPaymentVerifyObserver implements ObserverInterface
+{
+    /**
+     * @var \Magento\Sales\Model\OrderFactory $_orderFactory
+     */
+    protected $_orderFactory;
+    
+    /**
+     * @var \Magento\Checkout\Model\Session $_checkoutSession
+     */
+    protected $_checkoutSession;
+    
+    public function __construct(
+        \Magento\Sales\Model\OrderFactory $orderFactory,
+        \Magento\Checkout\Model\Session $checkoutSession
+    ) {
+        $this->_checkoutSession = $checkoutSession;
+        $this->_orderFactory = $orderFactory;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        //Observer execution code...
+        $order = $this->getOrder();
+        if ($order) {
+            // sets the status to processing since payment has been received
+            $order->setStatus(Order::STATE_PROCESSING);
+            $order->save();
+        }
+    }
+    
+    /**
+     * Loads the order based on the last real order
+     * @return boolean | 
+     */
+    private function getOrder()
+    {
+        // get the last real order id
+        $lastOrderId = $this->_checkoutSession->getLastRealOrderId();
+        if ($lastOrderId) {
+            // load and return the order instance
+            return $this->_orderFactory->create()->loadByIncrementId($lastOrderId);
+        }
+        return false;
+    }
+}
