46d9e6f811bda0a54fc1b8ca6909c05301f7d97c
Stephen Amaza <steveamaza@gmail.com>
2018-02-19 21:24:23 +0100

    Make changes to Paystack namespace

--- view/frontend/web/js/view/payment/method-renderer/pstk_paystack.js
+++ view/frontend/web/js/view/payment/method-renderer/pstk_paystack.js
@@ -1,125 +1,138 @@
 /*browser:true*/
 /*global define*/
 define(
-    [
-        'jquery',
-        'Magento_Checkout/js/view/payment/default',
-        'Magento_Checkout/js/action/place-order',
-        'Magento_Checkout/js/model/payment/additional-validators',
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/model/full-screen-loader',
-        'Magento_Checkout/js/action/redirect-on-success'
-    ],
-    function ($, Component, placeOrderAction, additionalValidators, quote, fullScreenLoader, redirectOnSuccessAction) {
-        'use strict';
+  [
+    "jquery",
+    "Magento_Checkout/js/view/payment/default",
+    "Magento_Checkout/js/action/place-order",
+    "Magento_Checkout/js/model/payment/additional-validators",
+    "Magento_Checkout/js/model/quote",
+    "Magento_Checkout/js/model/full-screen-loader",
+    "Magento_Checkout/js/action/redirect-on-success"
+  ],
+  function(
+    $,
+    Component,
+    placeOrderAction,
+    additionalValidators,
+    quote,
+    fullScreenLoader,
+    redirectOnSuccessAction
+  ) {
+    "use strict";
 
-        return Component.extend({
-            defaults: {
-                template: 'Pstk_Paystack/payment/form',
-                customObserverName: null
-            },
-            
-            redirectAfterPlaceOrder : false,
+    return Component.extend({
+      defaults: {
+        template: "Pstk_Paystack/payment/form",
+        customObserverName: null
+      },
 
-            initialize: function () {
-                this._super();
-                // Add Paystack Gateway script to head
-                $("head").append('<script src="https://js.paystack.co/v1/inline.js">');
-                return this;
-            },
+      redirectAfterPlaceOrder: false,
 
-            getCode: function () {
-                return 'pstk_paystack';
-            },
+      initialize: function() {
+        this._super();
+        // Add Paystack Gateway script to head
+        $("head").append('<script src="https://js.paystack.co/v1/inline.js">');
+        return this;
+      },
 
-            getData: function () {
-                return {
-                    'method': this.item.method,
-                    'additional_data': {}
-                };
-            },
+      getCode: function() {
+        return "pstk_paystack";
+      },
 
-            isActive: function () {
-                return true;
-            },
+      getData: function() {
+        return {
+          method: this.item.method,
+          additional_data: {}
+        };
+      },
 
-            /**
-             * @override
-             */
-            afterPlaceOrder: function () {
-                var checkoutConfig = window.checkoutConfig;
-                var paymentData = quote.billingAddress();
-                var paystackConfiguration = checkoutConfig.payment.pstk_paystack;
+      isActive: function() {
+        return true;
+      },
 
-                if (checkoutConfig.isCustomerLoggedIn) {
-                    var customerData = checkoutConfig.customerData;
-                    paymentData.email = customerData.email;
-                } else {
-                    var storageData = JSON.parse(localStorage.getItem('mage-cache-storage'))['checkout-data'];
-                    paymentData.email = storageData.validatedEmailValue;
+      /**
+       * @override
+       */
+      afterPlaceOrder: function() {
+        var checkoutConfig = window.checkoutConfig;
+        var paymentData = quote.billingAddress();
+        var paystackConfiguration = checkoutConfig.payment.pstk_paystack;
+
+        if (checkoutConfig.isCustomerLoggedIn) {
+          var customerData = checkoutConfig.customerData;
+          paymentData.email = customerData.email;
+        } else {
+          var storageData = JSON.parse(
+            localStorage.getItem("mage-cache-storage")
+          )["checkout-data"];
+          paymentData.email = storageData.validatedEmailValue;
+        }
+
+        var quoteId = checkoutConfig.quoteItemData[0].quote_id;
+
+        var _this = this;
+        _this.isPlaceOrderActionAllowed(false);
+        var handler = PaystackPop.setup({
+          key: paystackConfiguration.public_key,
+          email: paymentData.email,
+          amount: Math.ceil(quote.totals().grand_total * 100), // get order total from quote for an accurate... quote
+          phone: paymentData.telephone,
+          currency: checkoutConfig.totalsData.quote_currency_code,
+          metadata: {
+            quoteId: quoteId,
+            custom_fields: [
+              {
+                display_name: "QuoteId",
+                variable_name: "quote id",
+                value: quoteId
+              },
+              {
+                display_name: "Address",
+                variable_name: "address",
+                value: paymentData.street[0] + ", " + paymentData.street[1]
+              },
+              {
+                display_name: "Postal Code",
+                variable_name: "postal_code",
+                value: paymentData.postcode
+              },
+              {
+                display_name: "City",
+                variable_name: "city",
+                value: paymentData.city + ", " + paymentData.countryId
+              }
+            ]
+          },
+          callback: function(response) {
+            $.ajax({
+              method: "GET",
+              url:
+                paystackConfiguration.api_url +
+                "paystack/verify/" +
+                response.reference +
+                "_-~-_" +
+                quoteId
+            }).success(function(data) {
+              data = JSON.parse(data);
+
+              if (data.status) {
+                if (data.data.status === "success") {
+                  // redirect to success page after
+                  redirectOnSuccessAction.execute();
+                  return;
                 }
-                
-                var quoteId = checkoutConfig.quoteItemData[0].quote_id;
+              }
 
-                var _this = this;
-                _this.isPlaceOrderActionAllowed(false);
-                var handler = PaystackPop.setup({
-                  key: paystackConfiguration.public_key,
-                  email: paymentData.email,
-                  amount: Math.ceil(quote.totals().grand_total * 100), // get order total from quote for an accurate... quote
-                  phone: paymentData.telephone,
-                  currency: checkoutConfig.totalsData.quote_currency_code,
-                  metadata: {
-                     quoteId: quoteId,
-                     custom_fields: [
-                        {
-                         display_name: "QuoteId",
-                         variable_name: "quote id",
-                         value: quoteId
-                        },
-                        {
-                            display_name: "Address",
-                            variable_name: "address",
-                            value: paymentData.street[0] + ', ' + paymentData.street[1]
-                        },
-                        {
-                            display_name: "Postal Code",
-                            variable_name: "postal_code",
-                            value: paymentData.postcode
-                        },
-                        {
-                            display_name: "City",
-                            variable_name: "city",
-                            value: paymentData.city + ', ' + paymentData.countryId
-                        }
-                     ]
-                  },
-                  callback: function (response) {
-                        $.ajax({
-                            method: 'GET',
-                            url: paystackConfiguration.api_url + 'paystack/verify/' + response.reference + '_-~-_' + quoteId
-                        }).success(function (data) {
-                            
-                            data = JSON.parse(data);
-                            
-                            if (data.status) {
-                                if (data.data.status === 'success') {
-                                    // redirect to success page after
-                                    redirectOnSuccessAction.execute();
-                                    return;
-                                }
-                            }
-                            
-                            _this.isPlaceOrderActionAllowed(true);
-                            _this.messageContainer.addErrorMessage({
-                                message: "Error, please try again"
-                            });
-                                
-                        });
-                  }
-                });
-                handler.openIframe();
-            }
+              _this.isPlaceOrderActionAllowed(true);
+              _this.messageContainer.addErrorMessage({
+                message: "Error, please try again"
+              });
+            });
+          }
         });
-    }
+        handler.openIframe();
+      }
+    });
+  }
 );
